d77d07baf2265724e3bee987b35acc523b66a5db
leonardo alves <109185275+Leonardolasz@users.noreply.github.com>
2023-02-10 15:56:32 -0300

    Criação das models pedido, perfil e implementação de validador de cpf

--- pedido/admin.py
+++ pedido/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Pedido, ItemPedido
+
+
+class itemPedidoInline(admin.TabularInline):
+    model = ItemPedido
+    extra = 1
+
+
+class PedidoAdmin(admin.ModelAdmin):
+    inlines = [
+        itemPedidoInline
+    ]
+
+
+admin.site.register(Pedido, PedidoAdmin)
+admin.site.register(ItemPedido)
