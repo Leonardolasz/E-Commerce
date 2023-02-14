27aab4b7332e5da43e9ab695f8b2a1f1f35fcb3d
leonardo alves <109185275+Leonardolasz@users.noreply.github.com>
2023-02-14 14:57:34 -0300

    Criação das urls e listagem de produtos

--- produto/views.py
+++ produto/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.views import View
+from django.views.generic import ListView
 
-# Create your views here.
+from . import models
+
+
+class ListaProdutos(ListView):
+    model = models.Produto
+    template_name = 'produto/lista.html'
+    context_object_name = 'produtos'
+    paginate_by = 12
+
+
+class DetalheProduto(View):
+    pass
+
+
+class AddAoCarrinho(View):
+    pass
+
+
+class RemoverDoCarrinho(View):
+    pass
+
+
+class Carrinho(View):
+    pass
+
+
+class Finalizar(View):
+    pass
